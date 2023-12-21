6c72a204aa598d349b70597edd7d27d890ee92ad
Ayush0054 <ayush5aprill@gmail.com>
2023-12-21 11:34:54 +0530

    order confirmation page

---
+++ app/confirmation/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Chip,
+  Link,
+} from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import { useStore } from "../store";
+import CheckIcon from "../icons/checkIcon";
+
+function Page() {
+  const {
+    orderDetails,
+    paymentMethods,
+    fetchOrderDetails,
+    selectedPaymentMethod,
+    setSelectedPaymentMethod,
+  } = useStore();
+  const [variant, setVariant] = useState("");
+  const [result, setResult] = useState("");
+  const getRandomResult = () => {
+    const randomResult = Math.random() < 0.5 ? "failure" : "success";
+    setResult(randomResult);
+
+    setVariant(randomResult === "success" ? "success" : "danger");
+  };
+  useEffect(() => {
+    getRandomResult();
+  }, []);
+  return (
+    <div className="flex flex-col justify-center items-center m-24 ">
+      <Card className="md:w-full w-[350px] max-w-xl rounded-none shadow-none ">
+        <div className=" flex justify-center m-5">
+          <CheckIcon className="text-purple-700 w-16 h-16 border-2 border-purple-700 rounded-full p-2 " />
+        </div>
+        <CardHeader>
+          <h1 className=" font-bold text-2xl">Order Confirmation</h1>
+        </CardHeader>
+        <CardBody>
+          <div className="flex items-center justify-between mb-4">
+            <span className=" text-xl text-gray-600">Order ID:</span>
+            <span className=" text-gray-700 font-semibold">123456789</span>
+          </div>
+          <div className="flex justify-between mb-4">
+            <span className=" text-xl text-gray-600">Total Amount</span>
+            <span className="  font-semibold text-gray-700">
+              ${orderDetails.total + 5}
+            </span>
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <span className=" text-xl text-gray-600">Payment Method</span>
+            <div className="flex items-center space-x-2">
+              <span className=" text-gray-700 font-semibold">
+                {selectedPaymentMethod}
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className=" text-xl text-gray-600">Status</span>
+            <Chip
+              //@ts-ignore
+              color={variant}
+              className=" font-semibold "
+            >
+              {result}
+            </Chip>
+          </div>
+        </CardBody>
+        <CardFooter className="flex items-center justify-center">
+          <Button
+            color="secondary"
+            className=" md:text-lg text-sm font-semibold rounded-md "
+          >
+            CHECK STATUS
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default Page;
